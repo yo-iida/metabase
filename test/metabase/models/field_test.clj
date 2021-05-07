7f1f949d9b7c323b6112145af43d551eb6f5f10c
Cam Saul <github@camsaul.com>
2021-05-06 20:01:16 -0700

    DB and code migrations

--- test/metabase/models/field_test.clj
+++ test/metabase/models/field_test.clj
@@ -4,11 +4,11 @@
             [metabase.sync.analyze.classifiers.name :as name]))
 
 (deftest semantic-type-for-name-and-base-type-test
-  (doseq [[input expected] {["id"      :type/Integer] :type/PK
+  (doseq [[input expected] {["id"      :type/Integer] :Relation/PK
                             ;; other pattern matches based on type/regex (remember, base_type matters in matching!)
-                            ["rating"  :type/Integer] :type/Score
+                            ["rating"  :type/Integer] :Semantic/Score
                             ["rating"  :type/Boolean] nil
-                            ["country" :type/Text]    :type/Country
+                            ["country" :type/Text]    :Semantic/Country
                             ["country" :type/Integer] nil}]
     (testing (pr-str (cons 'semantic-type-for-name-and-base-type input))
       (is (= expected
